95391d4bbb548f98123c82859857a81ffee6887d
Mahaveer Singh Gurjar <mahaveer@opendatafabric.com>
2025-04-02 18:52:45 +0530

    added vercel analytics tab

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { RouterProvider } from 'react-router-dom';
 import { getRouter } from './routes';
 // import ParticlesAnimation from './components/ParticlesAnimation';
 import { Toaster } from 'react-hot-toast';
+import { Analytics } from '@vercel/analytics/react';
 function App() {
   const router = getRouter();
   return (
@@ -11,6 +12,7 @@ function App() {
         <RouterProvider router={router} />
       </div>
       <Toaster />
+      <Analytics />
     </div>
   );
 }
